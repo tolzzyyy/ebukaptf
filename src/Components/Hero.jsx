e4a5c37f058a7e7e5986df3f25eaf7b8d2d9a863
tolzzyyy <williamsegberongbe@gmail.com>
2024-07-19 12:11:44 +0100

    nineteenth commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/pagination';
 import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-
-
-import { FreeMode, Navigation, Autoplay } from 'swiper/modules';
+import { FreeMode, Autoplay } from 'swiper/modules';
 import ProjectPage from './ProjectPage';
 
 const Body = () => {
@@ -18,68 +14,57 @@ const Body = () => {
 
   return (
     <div>
-    <div className='max-w-[1640px] h-full  font-monument-extended mt-[40px] md:mt-[100px]  mx-auto p-4 px-[30px] lg:px-[40px] xl:px-[65px]'>
-      <div className=' relative'>
-        <Swiper
-          onSlideChange={(swiper) => handleSlideChange(swiper)}
-          breakpoints={{
-            340: {
-              slidesPerView: 1,
-              spaceBetween: 15,
-              direction: 'vertical'
-            },
-            700: {
-              slidesPerView: 1,
-              spaceBetween:15,
-              direction: 'vertical'
-            },
-          }}
-          navigation={{
-            clickable: true,
-            nextEl: '.swiper-button-nexts',
-            prevEl: '.swiper-button-prevs',
-          }}
-          freeMode={true}
-          modules={[FreeMode, Navigation, Autoplay]}
-          loop={true}
-          autoplay={{
-            delay: 3000, // Delay between slides in ms
-            disableOnInteraction: false,
-            allowTouchMove: false // Autoplay will not stop after user interactions
-          }}
-       
-          speed={1000}
-          effect='slide'
-          className="swiper-container lg:h-[400px] h-[200px] md:h-[300px] lg:w-full text-black"
-        >
-          <SwiperSlide>
-            <div className={`absolute w-full h-full text-black flex flex-col gap-5 justify-center  max-h-[400px] leading-[1.5] scale-${activeIndex === 0 ? '4' : '2'}`}>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] w-[200px] font-bold'>Slow </h1>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>Motion,</h1>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={`absolute w-full h-full text-black flex flex-col gap-5 justify-center max-h-[400px] leading-[1.5] scale-${activeIndex === 1 ? '2' : '3'}`}>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>IS <br /> </h1>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'> Better</h1>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'> Than</h1>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={`absolute w-full h-full text-black flex flex-col gap-5 justify-center max-h-[400px] leading-[1.5] scale-${activeIndex === 2 ? '3' : '4'}`}>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>No  </h1>
-              <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>Motion.</h1>
-            </div>
-          </SwiperSlide>
-          {/* Add more SwiperSlides here */}
-        </Swiper>
+      <div className='max-w-[1640px] h-full font-monument-extended mt-[40px] md:mt-[100px] mx-auto p-4 px-[30px] lg:px-[40px] xl:px-[65px]'>
+        <div className='relative'>
+          <Swiper
+            onSlideChange={(swiper) => handleSlideChange(swiper)}
+            breakpoints={{
+              340: {
+                slidesPerView: 1,
+                spaceBetween: 15,
+                direction: 'vertical'
+              },
+              700: {
+                slidesPerView: 1,
+                spaceBetween: 15,
+                direction: 'vertical'
+              },
+            }}
+            freeMode={true}
+            modules={[FreeMode, Autoplay]}
+            loop={true}
+            autoplay={{
+              delay: 3000, // Delay between slides in ms
+              disableOnInteraction: false,
+            }}
+            speed={1000}
+            effect='slide'
+            className="swiper-container lg:h-[400px] h-[200px] md:h-[300px] lg:w-full text-black"
+          >
+            <SwiperSlide>
+              <div className={`absolute w-full h-full text-black flex flex-col gap-5 justify-center max-h-[400px] leading-[1.5] scale-${activeIndex === 0 ? '4' : '2'}`}>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] w-[200px] font-bold'>Slow </h1>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>Motion,</h1>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={`absolute w-full h-full text-black flex flex-col gap-5 justify-center max-h-[400px] leading-[1.5] scale-${activeIndex === 1 ? '2' : '3'}`}>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>IS <br /> </h1>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'> Better</h1>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'> Than</h1>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={`absolute w-full h-full text-black flex flex-col gap-5 justify-center max-h-[400px] leading-[1.5] scale-${activeIndex === 2 ? '3' : '4'}`}>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>No  </h1>
+                <h1 className='px-4 text-4xl sm:text-4xl md:text-5xl lg:text-[100px] font-bold'>Motion.</h1>
+              </div>
+            </SwiperSlide>
+            {/* Add more SwiperSlides here */}
+          </Swiper>
+        </div>
       </div>
-
-      {/* <div>
-        <ProjectPage/>
-      </div> */}
-    </div>
-    <ProjectPage/>
+      <ProjectPage />
     </div>
   );
 }
