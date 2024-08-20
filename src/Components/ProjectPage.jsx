9c06fb9478b9f0b61ccb949bc011d58258ef2540
tolzzyyy <williamsegberongbe@gmail.com>
2024-08-20 12:17:02 +0100

    update

--- src/Components/ProjectPage.jsx
+++ src/Components/ProjectPage.jsx
@@ -3,6 +3,7 @@ import guardians from "../Assets/guardians.png";
 import brimble from "../Assets/brimble.png";
 import brimble2 from "../Assets/brimble2.png";
 import nft from "../Assets/nft.png";
+import tola from '../Assets/Rectangle 60.png'
 import bdomain from "../Assets/bdomain.png";
 import oxellus from "../Assets/oxellus.png";
 import { BsBehance } from "react-icons/bs";
@@ -30,20 +31,32 @@ const ProjectPage = () => {
               <p className="text-[#737373] text-[9px] lg:text-[12px]">Identity and motion design</p>
             </div>
           </Link>
+         
+          <div className="relative group">
+  <div className="relative cursor-pointer">
+    {/* Vertical Bar */}
+    <div className="absolute top-0 h-full bg-blue-500 z-50 transition-all duration-700 ease-in-out  w-0 group-hover:w-[20px]"></div>
+    
+    {/* Image Content */}
+    <img src={tola} alt="Portfolio Preview" className="w-full h-auto" />
 
-          {/* Card 2 */}
-          <Link to="/nft" className="relative group">
-            <div className="relative">
-              <div className="absolute top-0 h-full bg-blue-500 transition-all duration-700 ease-in-out w-0 group-hover:w-[20px]"></div>
-              <img src={nft} className="w-full h-auto" alt="" />
-            </div>
-            <div className="flex flex-col mt-3">
-              <h1 className="transition-all duration-700 text-[13px] lg:text-[9px] xl:text-[21px] ease-in-out group-hover:text-blue-500">
-                NFT ng
-              </h1>
-              <p className="text-[#737373] text-[9px] lg:text-[12px]">Identity, Packaging and experience design</p>
-            </div>
-          </Link>
+    {/* Coming Soon Text with Blur on Top */}
+    <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-[30px] backdrop-blur-[5px] transition-all duration-700 ease-in-out opacity-0 group-hover:opacity-100">
+      <h2 className="md:text-[30px] text-[20px] text-black font-bold">Coming Soon</h2>
+    </div>
+  </div>
+
+  <div className="flex flex-col mt-3">
+    <h1 className="transition-all duration-700 text-[13px] lg:text-[9px] xl:text-[21px] ease-in-out group-hover:text-blue-500">
+      Adetola - Portfolio
+    </h1>
+    <p className="text-[#737373] text-[9px] lg:text-[12px]">UX and Product Design</p>
+  </div>
+</div>
+
+
+
+         
 
           {/* Card 3 */}
           <Link to='/bimble2' className="relative group">
@@ -58,6 +71,20 @@ const ProjectPage = () => {
               <p className="text-[#737373] text-[9px] lg:text-[12px]">Motion design and Illustration</p>
             </div>
           </Link>
+           {/* Card 2 */}
+           <Link to="/nft" className="relative group">
+            <div className="relative">
+              <div className="absolute top-0 h-full bg-blue-500 transition-all duration-700 ease-in-out w-0 group-hover:w-[20px]"></div>
+              <img src={nft} className="w-full h-auto" alt="" />
+            </div>
+            <div className="flex flex-col mt-3">
+              <h1 className="transition-all duration-700 text-[13px] lg:text-[9px] xl:text-[21px] ease-in-out group-hover:text-blue-500">
+                NFT ng
+              </h1>
+              <p className="text-[#737373] text-[9px] lg:text-[12px]">Identity, Packaging and experience design</p>
+            </div>
+          </Link>
+
           <Link to="/bimbledomain" className="relative group">
             <div className="relative">
               <div className="absolute top-0 h-full bg-blue-500 transition-all duration-700 ease-in-out w-0 group-hover:w-[20px]"></div>
@@ -70,20 +97,6 @@ const ProjectPage = () => {
               <p className="text-[#737373] text-[9px] lg:text-[12px]">Motion design and art direction</p>
             </div>
           </Link>
-
-          {/* Card 4 */}
-          <div className="relative group">
-            <div className="relative">
-              <div className="absolute top-0 h-full bg-blue-500 transition-all duration-700 ease-in-out w-0 group-hover:w-[20px]"></div>
-              <img src={guardians} className="w-full h-auto" alt="" />
-            </div>
-            <div className="flex flex-col mt-3">
-              <h1 className="transition-all duration-700 text-[13px] lg:text-[9px] xl:text-[21px] ease-in-out group-hover:text-blue-500">
-                Guild of Guardians Cinematic
-              </h1>
-              <p className="text-[#737373] text-[9px] lg:text-[12px]">Identity, Packaging and experience design</p>
-            </div>
-          </div>
 
           {/* Card 5 */}
           {/* <Link to="/bimbledomain" className="relative group">
